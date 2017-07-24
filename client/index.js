cceaf97b16e3d6a03c9e9653e7ce6be94bc3e838
Danni Liu <dannielleorlando@gmail.com>
2017-07-24 15:46:47 -0400

    changed

--- client/index.js
+++ client/index.js
@@ -1,7 +1,14 @@
+'use strict';
+
+import { Provider } from 'react-redux';
+import store from './store';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 ReactDOM.render(
-<div>Hello world! BY DANNI AND ALICE</div>,
-    document.getElementById('app') // make sure this is the same as the id of the div in your index.html
+  <Provider store={store}>
+    {/* rest of your app goes here! */}
+  </Provider>,
+  document.getElementById('app')
 );
--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, '..', 'node_modules')));
 app.use('/api',require('./api'));
 
 app.get('*', function (req, res, next) {
-    res.sendFile(path.join(__dirname, '../index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 app.use(function (err, req, res, next) {
